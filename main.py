dc8869ffa09045117f9d03ccaeffc1f63f858dc8
mygit20031984 <ashuklkrn@gmail.com>
2020-12-16 19:57:17 +0530

    Commit

--- main.py
+++ main.py
@@ -163,22 +163,55 @@ def get_auc_scores(y_actual, method,method2):
 # best_model(xgb_grid)
 #########################################################################################
 #########################################################################################
-#########################################################################################
-#########################################################################################
-#########################################################################################
 #Fit Best Model
-# Fit logistic regression with degree 2 polynomial kernel
-# param_grid = {'C': [0.1,10,50], 'max_iter': [300,500], 'fit_intercept':[True],'intercept_scaling':[1],'penalty':['l2'],
-#               'tol':[0.0001,0.000001]}
-# poly2 = PolynomialFeatures(degree=2)
-# df_train_pol2 = poly2.fit_transform(df_train.loc[:, df_train.columns != 'Exited'])
-# log_pol2_Grid = GridSearchCV(LogisticRegression(solver = 'liblinear'),param_grid, cv=5, refit=True, verbose=0)
-# log_pol2_Grid.fit(df_train_pol2,df_train.Exited)
-# best_model(log_pol2_Grid)
-
+# Fit primal logistic regression
+log_primal = LogisticRegression(C=100, class_weight=None, dual=False, fit_intercept=True,intercept_scaling=1, max_iter=250, multi_class='auto',n_jobs=None,
+                                penalty='l2', random_state=None, solver='lbfgs',tol=1e-05, verbose=0, warm_start=False)
+log_primal.fit(df_train.loc[:, df_train.columns != 'Exited'],df_train.Exited)
+#########################################################################################
 # Fit logistic regression with pol 2 kernel
 poly2 = PolynomialFeatures(degree=2)
 df_train_pol2 = poly2.fit_transform(df_train.loc[:, df_train.columns != 'Exited'])
 log_pol2 = LogisticRegression(C=10, class_weight=None, dual=False, fit_intercept=True,intercept_scaling=1, max_iter=300, multi_class='auto', n_jobs=None,
                               penalty='l2', random_state=None, solver='liblinear',tol=0.0001, verbose=0, warm_start=False)
 log_pol2.fit(df_train_pol2,df_train.Exited)
+# Fit SVM with RBF Kernel
+SVM_RBF = SVC(C=100, cache_size=200, class_weight=None, coef0=0.0, decision_function_shape='ovr', degree=3, gamma=0.1, kernel='rbf', max_iter=-1, probability=True,
+              random_state=None, shrinking=True,tol=0.001, verbose=False)
+SVM_RBF.fit(df_train.loc[:, df_train.columns != 'Exited'],df_train.Exited)
+#########################################################################################
+# Fit SVM with Pol Kernel
+SVM_POL = SVC(C=100, cache_size=200, class_weight=None, coef0=0.0,  decision_function_shape='ovr', degree=2, gamma=0.1, kernel='poly',  max_iter=-1,
+              probability=True, random_state=None, shrinking=True, tol=0.001, verbose=False)
+SVM_POL.fit(df_train.loc[:, df_train.columns != 'Exited'],df_train.Exited)
+#########################################################################################
+# Fit Random Forest classifier
+RF = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',max_depth=8, max_features=6, max_leaf_nodes=None,min_impurity_decrease=0.0,
+                            min_impurity_split=None,min_samples_leaf=1, min_samples_split=3,min_weight_fraction_leaf=0.0, n_estimators=50, n_jobs=None,
+                            oob_score=False, random_state=None, verbose=0,warm_start=False)
+RF.fit(df_train.loc[:, df_train.columns != 'Exited'],df_train.Exited)
+#########################################################################################
+# Fit Extreme Gradient Boost Classifier
+XGB = XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,colsample_bytree=1, gamma=0.01, learning_rate=0.1, max_delta_step=0,max_depth=7,
+                    min_child_weight=5, missing=None, n_estimators=20,n_jobs=1, nthread=None, objective='binary:logistic', random_state=0,reg_alpha=0,
+                    reg_lambda=1, scale_pos_weight=1, seed=None, silent=True, subsample=1)
+XGB.fit(df_train.loc[:, df_train.columns != 'Exited'],df_train.Exited)
+#########################################################################################
+print(classification_report(df_train.Exited, log_primal.predict(df_train.loc[:, df_train.columns != 'Exited'])))
+print(classification_report(df_train.Exited,  log_pol2.predict(df_train_pol2)))
+print(classification_report(df_train.Exited,  SVM_RBF.predict(df_train.loc[:, df_train.columns != 'Exited'])))
+print(classification_report(df_train.Exited,  SVM_POL.predict(df_train.loc[:, df_train.columns != 'Exited'])))
+print(classification_report(df_train.Exited,  RF.predict(df_train.loc[:, df_train.columns != 'Exited'])))
+print(classification_report(df_train.Exited,  XGB.predict(df_train.loc[:, df_train.columns != 'Exited'])))
+
+y = df_train.Exited
+X = df_train.loc[:, df_train.columns != 'Exited']
+X_pol2 = df_train_pol2
+auc_log_primal, fpr_log_primal, tpr_log_primal = get_auc_scores(y, log_primal.predict(X),log_primal.predict_proba(X)[:,1])
+auc_log_pol2, fpr_log_pol2, tpr_log_pol2 = get_auc_scores(y, log_pol2.predict(X_pol2),log_pol2.predict_proba(X_pol2)[:,1])
+auc_SVM_RBF, fpr_SVM_RBF, tpr_SVM_RBF = get_auc_scores(y, SVM_RBF.predict(X),SVM_RBF.predict_proba(X)[:,1])
+auc_SVM_POL, fpr_SVM_POL, tpr_SVM_POL = get_auc_scores(y, SVM_POL.predict(X),SVM_POL.predict_proba(X)[:,1])
+auc_RF, fpr_RF, tpr_RF = get_auc_scores(y, RF.predict(X),RF.predict_proba(X)[:,1])
+auc_XGB, fpr_XGB, tpr_XGB = get_auc_scores(y, XGB.predict(X),XGB.predict_proba(X)[:,1])
+
+
